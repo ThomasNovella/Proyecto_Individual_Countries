50b3395023277ba729cb4eb0c6aab03af518e9ed
ThomasNovella <thomasnovella12@gmail.com>
2023-04-21 17:40:03 -0300

    finishing the models

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,46 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
-    name: {
+   id: {
+      type: DataTypes.STRING(3),
+      primaryKey: true,
+   },
+   name: {
       type: DataTypes.STRING,
+      allowNull: false
+   },
+   flagImg: {
+      type: DataTypes.STRING,
+      allowNull: false
+   },
+   continent: {
+      type: DataTypes.ENUM(
+        "Asia",
+        "Europe",
+        "North America",
+        "South America",
+        "Ocenia",
+        "Antartica",
+        "Africa"
+      ),
       allowNull: false,
-    },
-  });
+   },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false
+   },
+   subregion: {
+      type: DataTypes.STRING,
+      allowNull: true
+  },
+   area: {
+      type: DataTypes.INTEGER,
+// no puse allowNull porque ya viene seteado como true por default
+  },
+  population: {
+      type: DataTypes.INTEGER,
+    }
+  },
+  {timestamps:false}
+  );
 };
